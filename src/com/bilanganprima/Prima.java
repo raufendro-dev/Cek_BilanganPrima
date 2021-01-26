a9e5202b02295b94a73599784bb937d28e1b5c06
raufendro-dev <raufendro@gmail.com>
2021-01-26 17:22:15 +0700

    First commit

---
+++ src/com/bilanganprima/Prima.java
@@ -0,0 +1,26 @@
+package com.bilanganprima;
+
+import java.util.Scanner;
+
+public class Prima {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+        int bilangan;
+        int cek=0;
+        bilangan=input.nextInt();
+        for (int i=2; i<=bilangan; i++) {
+        	if(bilangan%i==0) {
+        		cek=cek+1;
+        	}
+        }
+        System.out.println("Bisa dibagi dengan bilangan lain sebanyak "+cek+" bilangan");
+        if(cek==1) {
+        	System.out.println(bilangan+" adalah bilangan prima");
+        } else {
+        	System.out.println(bilangan+" bukan bilangan prima");
+        }
+	}
+
+}
