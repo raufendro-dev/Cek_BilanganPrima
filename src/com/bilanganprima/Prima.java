18d7bbbd9c2738a7504f5a9c1c4a36966784bee6
raufendro-dev <raufendro@gmail.com>
2021-01-29 12:45:02 +0700

    Update

--- src/com/bilanganprima/Prima.java
+++ src/com/bilanganprima/Prima.java
@@ -10,12 +10,17 @@ public class Prima {
         int bilangan;
         int cek=0;
         bilangan=input.nextInt();
+        System.out.println("Bilangan bisa dibagi dengan beberapa bilangan berikut :");
         for (int i=2; i<=bilangan; i++) {
         	if(bilangan%i==0) {
+        		
+        		System.out.println(i);
         		cek=cek+1;
         	}
         }
+        System.out.println("");
         System.out.println("Bisa dibagi dengan bilangan lain sebanyak "+cek+" bilangan");
+        System.out.println("===========================================================");
         if(cek==1) {
         	System.out.println(bilangan+" adalah bilangan prima");
         } else {
